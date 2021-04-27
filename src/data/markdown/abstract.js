68a2cb784a59bab7679e8bb43d0acb0849cc3393
Elaine <elaine13@email.unc.edu>
2021-04-26 22:12:08 -0400

    syntax fix

--- src/data/markdown/abstract.js
+++ src/data/markdown/abstract.js
@@ -1,4 +1,4 @@
-const abstract = `Hello, I'm Elaine.  
+const abstract = `Hello, I'm Elaine!  
 I'm a computer science student at UNC. I enjoy impressionist painting, learning languages, and playing video games.
 `;
 
