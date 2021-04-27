2d56d5c5899e89c4b1e95202cf11c56f230190b4
Elaine <elaine13@email.unc.edu>
2021-04-26 21:05:21 -0400

    added information

---
+++ src/data/markdown/competitions.js
@@ -0,0 +1,65 @@
+const competitions = `# So You Want to Learn About Figure Skating Competitions
+
+**What are the main competitions?**
+The major *international* competiitons sanctioned by the International Skating Union are as follows:
+- Olympics (every four years)
+- World Championships (annual)
+- European Championships (annual)
+- Four Continents (annual)
+- Grand Prix of Figure Skating (annual)
+- Challenger Series (varying)
+- World Team Trophy (annual)
+
+The major *national* competitions are sanctioned by individual countries' governing skating federations.
+
+## Olympic Championships
+
+Considered the most important competition of an elite figure skater's career, this event is held every four years at the Winter Olympic Games.
+
+The current reigning Olympic champions are:
+- Men's Singles: Yuzuru Hanyu (Japan)
+- Ladies' Singles: Alina Zagitova (Olympic Athletes from Russia)
+- Pair's: Aljona Savchenko and Bruno Massot (Germany)
+- Ice Dance: Tessa Virtue and Scott Moir (Canada)
+- Team: Patrick Chan, Kaetlyn Osmond, Gabrielle Daleman, Meagan Duhamel / Eric Radford, Tessa Virtue / Scott Moir (Canada)
+
+## World Championships
+
+As the most important annual competition, it takes place in March at the end of the competitive season.
+
+## European Championships
+
+This competition is exclusively for competitors from European countries.
+
+## Four Continents
+
+This competition is exclusively for competitors outside of Europe
+
+## Grand Prix of Figure Skating
+
+The Grand Prix is an invitational series of competitions. Competitors are invited to two of six of the following competitions:
+- Skate America
+- Skate Canada International
+- Cup of China
+- Internationaux de France
+- Rostelecom Cup
+- NHK Trophy
+Skaters gain points based on their performance at each event. The six skaters or pairs with the most points in each discipline is then invited to the Grand Prix Final.
+
+## The Challenger Series
+
+This is a series of minor competitions including but not limited to:
+- Nebelhorn Trophy (Germany)
+- Ondrej Nepela Memorial (Slovakia)
+- Finlandia Trophy (Finland)
+- Autumn Classic (Canada)
+- Warsaw Cup (Poland)
+- Golden Spin of Zagreb (Croatia)
+
+## World Team Trophy
+
+This is a national team-based event that takes place in Japan. The six countries with the best performance of the season send two men, two ladies, one pair, and one ice dancing pair to the event. The qualifiying countries have commonly been the US, Canada, Japan, France, Russia, and China.
+
+`;
+
+export default competitions;
