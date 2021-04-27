4bcd63c36cda93fb6ab766722cb990d3dd7b02f2
Elaine <elaine13@email.unc.edu>
2021-04-26 22:27:34 -0400

    changed front page

--- src/data/markdown/index.js
+++ src/data/markdown/index.js
@@ -4,3 +4,4 @@ export { default as lesson_three } from "./lesson_three.js";
 export { default as lesson_four } from "./lesson_four.js";
 export { default as frontpage } from "./frontpage";
 export { default as competitions } from "./competitions";
+export { default as abstract } from "./abstract";
