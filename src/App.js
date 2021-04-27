9d764ee5a61c772f81b562873051ce1cad530b9c
Elaine <elaine13@email.unc.edu>
2021-04-26 22:25:34 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import ReactMarkdown from "react-markdown";
-import { abstract } from "./data/markdown";
+import { frontpage } from "./data/markdown";
 
 const App = () => {
   return (
     <div className="home">
       <div id="content">
-        <ReactMarkdown source={abstract} />
+        <ReactMarkdown source={frontpage} />
       </div>
     </div>
   );
