ed88eea017e5014c6bf477990c510ef196aa5ef1
Elaine Dong <elaine13@email.unc.edu>
2022-08-23 17:16:21 -0400

    test

--- src/data/markdown/abstract.js
+++ src/data/markdown/abstract.js
@@ -1,5 +1,4 @@
 const abstract = `Hello, I'm Elaine!  
-I'm a computer science student at UNC. I enjoy impressionist painting, learning languages, and playing video games.
 `;
 
 export default abstract;
--- src/data/markdown/frontpage.js
+++ src/data/markdown/frontpage.js
@@ -1,10 +1,5 @@
 const frontpage = `# Introduction
 
-Welcome to So You Want to Learn About Figure Skating.
-
-**What is figure skating?**
-It's a sport where individuals, pairs, or groups perform on figure skates on ice. The most well-known disciplines are men's singles, ladies' singles, pair skating, and ice dance.
-
 `;
 
 export default frontpage;
