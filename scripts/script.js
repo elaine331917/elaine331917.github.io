a983f8e390e876e8c92bfb2fb1950d51a4795bcf
Elaine Dong <elaine13@email.unc.edu>
2023-05-02 13:10:55 -0400

    reorganize

--- scripts/script.js
+++ scripts/script.js
@@ -37,27 +37,4 @@ for (var i = 0; i < email.length; i++) {
     encodedEmail += '&#' + email.charCodeAt(i) + ';';
 }
 
-// Skill list
-
-const programming = ["TypeScript", "HTML", "CSS", "SQL", "Python", "JavaScript", "Java"];
-const frameworks = ["Angular", "Bootstrap", "FastAPI", "SQLAlchemy", "RxJS"]
-const tools = ["Figma", "Git & GitHub", "DevTools", "PostgreSQL"]
-const design = ["wireframing", "prototyping", "usability testing", "user research", "graphic design"]
-
-const programmingList = document.getElementById("programming");
-const frameworksList = document.getElementById("frameworks");
-const toolsList = document.getElementById("tools");
-const designList = document.getElementById("design");
-
-function populateList(array, element) {
-    array.forEach((item)=>{
-        let li = document.createElement("li");
-        li.innerText = item;
-        element.appendChild(li);
-    })
-}
-
-populateList(programming, programmingList);
-populateList(frameworks, frameworksList);
-populateList(tools, toolsList);
-populateList(design, designList);
+document.getElementById('email').innerHTML = encodedEmail;
