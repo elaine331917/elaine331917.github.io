d50d7f69478f9efd62b34a8f6dc2e9c88370e4b2
Elaine Dong <elaine13@email.unc.edu>
2024-02-26 14:16:06 -0500

    format art gallery page

--- src/components/artwork.tsx
+++ src/components/artwork.tsx
@@ -6,9 +6,19 @@ interface ArtworkProps {
 
 export default function Artwork({ art }: ArtworkProps) {
     return (
-        <div>
-            <img src={art.image} alt={art.title} className="w-full h-auto"/>
-            <div>{art.title}, {art.date}</div>
+        <div className="relative group">
+            <div className="rounded-xl overflow-hidden">
+                <img
+                    src={art.image}
+                    alt={art.title}
+                    className="w-full h-auto transition-transform duration-300 transform-gpu group-hover:scale-105"
+                />
+            </div>
+            <div className="absolute rounded-xl overflow-hidden inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex justify-start items-end transition-opacity duration-300 p-5">
+                <div className="text-white text-center">
+                    <h2 className="text-lg font-bold">{art.title}</h2>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -8,7 +8,7 @@ interface ColumnProps {
 
 function Column({ artworks }: ColumnProps) {
     return (
-        <div>
+        <div className="flex flex-col gap-5">
             {artworks.map((artwork, index) => (
                 <Artwork
                 key={`work-${index}`}
@@ -25,9 +25,9 @@ export default function Gallery() {
         <div className="page-container">
             <section>
                 <div className="subtitle">
-                    <div className="subheading">My Art</div>
+                    <div className="subheading mb-5">My Art</div>
                 </div>
-                <div className="grid md:grid-cols-2 lg:grid-cols-3">
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
                     <Column artworks={works.slice(0,4)}></Column>
                     <Column artworks={works.slice(4,7)}></Column>
                     <Column artworks={works.slice(7,10)}></Column>
